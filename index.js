ce5772dd44baae6b8a222afc72d21a463aaf79e8
Misha <mtwstflr@gmail.com>
2017-07-27 23:11:11 +0000

    Added functioning /start command

--- index.js
+++ index.js
@@ -2,15 +2,18 @@
 	var token = '372781050:AAE4SWncReJrOpHV2hYlFcbTZGuFvdA2EDU';
     
     var bot = new TelegramBot(token, {polling: true});
-bot.onText(/\/echo (.+)/, function (msg, match) {
-      const fromId = msg.from.id;
-      const resp = match[1];
-      bot.sendMessage(fromId, resp);
+	
+
+	bot.onText(/\/start/, function (msg, match) {
+      	const fromId = msg.from.id;
+	const name = msg.from.first_name;
+	const messageStart = "Привет, " + name + "! Я -- простой бот, который поможет вам отправить данные по вашему рейсу электричек";
+      bot.sendMessage(fromId, messageStart);
     });
 
-	bot.on('message', function(msg) {
-  const chatId = msg.chat.id;
+	bot.onText(/\/hi/, function (msg, match) {
+		const fromId = msg.from.id;
+		bot.sendMessage(fromId, "Привет!");
+	})
 
-  // send a message to the chat acknowledging receipt of their message
-  bot.sendMessage(chatId, 'Received your message');
-});
+
