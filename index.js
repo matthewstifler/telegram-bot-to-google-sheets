7297b4953023d3685a702b2ab04e7a2e04d2de64
Misha <mtwstflr@gmail.com>
2017-07-28 00:08:09 +0000

    Added keyboard and base for reply handler

--- index.js
+++ index.js
@@ -7,9 +7,20 @@
 	bot.onText(/\/start/, function (msg, match) {
       	const fromId = msg.from.id;
 	const name = msg.from.first_name;
-	const messageStart = "Привет, " + name + "! Я -- простой бот, который поможет вам отправить данные по вашему рейсу электричек";
-      bot.sendMessage(fromId, messageStart);
-    });
+	const messageStart = "Привет, " + name + "! Я -- простой бот, который поможет вам отправить данные по вашему рейсу электричек. Выберите число:";
+     var messageSent =  bot.sendMessage(fromId, messageStart, {
+		"reply_markup": {
+			"keyboard": [["1", "2", "3"], ["4", "5", "6"], ["7", "8", "9"]],
+			"one_time_keyboard": true
+		}
+	});
+
+	
+	
+	bot.onText(/\d+/, function(msg, match) {
+		bot.sendMessage(msg.from.id, "You replied: " + match[0] + " on " + new Date(msg.date));
+	})
+    });  
 
 	bot.onText(/\/hi/, function (msg, match) {
 		const fromId = msg.from.id;
